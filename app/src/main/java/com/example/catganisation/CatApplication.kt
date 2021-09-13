a716fcbc18572e69eeb4714287bb621ac7285e3f
Ionut <faurionnut@gmail.com>
2021-09-13 15:40:10 +0300

    package structure

---
+++ app/src/main/java/com/example/catganisation/CatApplication.kt
@@ -0,0 +1,5 @@
+package com.example.catganisation
+
+import android.app.Application
+
+class CatApplication : Application()
--- app/src/main/java/com/example/catganisation/presentation/MainActivity.kt
+++ app/src/main/java/com/example/catganisation/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.catganisation
+package com.example.catganisation.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.catganisation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
