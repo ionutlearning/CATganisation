3e09aeacff9cde33f06a097206f343a9f2d76d8e
Ionut <faurionnut@gmail.com>
2021-09-18 20:13:23 +0300

    update main activity

--- app/src/main/java/com/example/catganisation/presentation/MainActivity.kt
+++ app/src/main/java/com/example/catganisation/presentation/MainActivity.kt
@@ -2,14 +2,23 @@ package com.example.catganisation.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.catganisation.R
+import com.example.catganisation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
