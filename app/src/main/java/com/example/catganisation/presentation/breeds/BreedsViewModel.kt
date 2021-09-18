f115157f8f2e1a86bdb5a1a10aa52a7f3e964995
Ionut <faurionnut@gmail.com>
2021-09-18 03:09:46 +0300

    v3

--- app/src/main/java/com/example/catganisation/presentation/breeds/BreedsViewModel.kt
+++ app/src/main/java/com/example/catganisation/presentation/breeds/BreedsViewModel.kt
@@ -1,15 +1,12 @@
 package com.example.catganisation.presentation.breeds
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.catganisation.domain.ViewResult
 import com.example.catganisation.domain.model.Breed
 import com.example.catganisation.domain.usecase.breeds.GetBreedsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,13 +14,9 @@ import javax.inject.Inject
 @HiltViewModel
 class BreedsViewModel @Inject constructor(useCase: GetBreedsUseCase) : ViewModel() {
 
-    private val _state = MutableLiveData<ViewResult<List<Breed>>>()
-    val state: LiveData<ViewResult<List<Breed>>> = _state
-
-    init {
-        _state.postValue(ViewResult.Loading)
-        viewModelScope.launch(Dispatchers.IO) {
-            useCase().collect { _state.postValue(it) }
-        }
-    }
+    val state: LiveData<ViewResult<List<Breed>>> = useCase()
+        .flowOn(Dispatchers.IO)
+        .onStart { emit(ViewResult.Loading) }
+        .catch { emit(ViewResult.Error(it.localizedMessage ?: "Unknown error")) }
+        .asLiveData()
 }
